300e2d0b007938bc5d0619cc0b90ce738f9067ec
miku.ito <miku.ito@rakus-partners.co.jp>
2026-03-02 11:41:49 +0900

    ロジック+Springテスト

---
+++ spring-exam/exam-web-spring-thymeleaf/src/main/java/com/example/q8/BaseballController.java
@@ -0,0 +1,36 @@
+package com.example.q8;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class BaseballController {
+	
+	@Autowired
+	private BaseballService service;
+	
+	@RequestMapping(value="/q8")
+	public String gotoq8(Model model){
+		
+		List<BaseballDomain> baseballList = service.findAll();			//元data
+
+		model.addAttribute("teams", baseballList);
+		
+		List<BaseballDomain> baseballList2 = service.findStadium();			//元data
+
+		model.addAttribute("stadiums", baseballList2);
+		
+		return "8/list";
+	}
+	
+	
+
+	
+	
+	
+}
